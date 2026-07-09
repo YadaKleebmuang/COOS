afedfba2e6c82cff3ea9b6a4b2845524801d8837
YadaKleebmuang <yadakleebmuang@gmail.com>
2026-07-09 22:53:52 +0700

    feat: initialize database connection and implement packages and workTypes API routes

--- backend/src/routes/v1/workTypes.route.js
+++ backend/src/routes/v1/workTypes.route.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/workTypeController");
+const authMiddleware = require("../../middlewares/auth.middleware");
 const adminOnly = require("../../middlewares/admin.middleware");
 
 // routes สำหรับ work types
 router.get("/", controller.getAll);
 
 // Admin only
-router.post("/", adminOnly, controller.create);
-router.patch("/:id", adminOnly, controller.update);
-router.delete("/:id", adminOnly, controller.remove);
+router.post("/", authMiddleware, adminOnly, controller.create);
+router.patch("/:id", authMiddleware, adminOnly, controller.update);
+router.delete("/:id", authMiddleware, adminOnly, controller.remove);
 
 module.exports = router;
